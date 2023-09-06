6f3b4bb53d44df507ec68b59022d4e092e864fa8
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-09-06 18:41:32 -0400

    Complete

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from datetime import datetime
 
 from models import db, Message
 
@@ -14,13 +15,40 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods = ['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at).all()
+        return jsonify([message.to_dict() for message in messages]), 200
 
-@app.route('/messages/<int:id>')
+    elif request.method == 'POST':
+        body = request.json.get('body')
+        username = request.json.get('username')
+        new_message = Message(body=body, username=username)
+        db.session.add(new_message)
+        db.session.commit()
+        return jsonify(new_message.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods = ['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.get(id)
+    if message is None:
+        return jsonify({"error": "Message not found"}), 404
+
+    if request.method == 'GET':
+        return jsonify(message.to_dict()), 200
+
+    elif request.method == 'PATCH':
+        new_body = request.json.get('body')
+        if new_body:
+            message.body = new_body
+            db.session.commit()
+        return jsonify(message.to_dict()), 200
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return jsonify({"message": "Message successfully deleted"}), 200
 
 if __name__ == '__main__':
     app.run(port=5555)
